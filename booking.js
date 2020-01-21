4c99229635e50b56967af32d404bf9ef171c5ef1
Tom <tom@homeflow.co.uk>
2020-01-21 14:16:59 +0000

    Add calendar functionality

---
+++ booking.js
@@ -0,0 +1,12 @@
+var times = [];
+var hours = 24 
+for(var i=0; i<=hours; i++) {
+  times.push(i);
+} 
+
+function buildTable() {
+  [0, 1, 2, 3, 4, 5].forEach(function(){
+    times.forEach(function(){
+     });
+  });
+};
